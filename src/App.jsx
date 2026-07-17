7811144dd162fb026e3042cdf864756b95d3abe4
Andrea Pasin <apasin@Andreas-MacBook-Air.local>
2026-07-17 22:52:46 +1000

    Adding cloudflare pages support for migration

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, Certs } from "./components";
 
+// Checking if the current host contains "pages.dev" (Cloudflare)
+const isCloudflare = window.location.hostname.includes('pages.dev');
+const routerBasename = isCloudflare ? '/' : '/xeno';
+
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={routerBasename}>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
